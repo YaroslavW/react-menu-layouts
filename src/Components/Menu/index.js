68bc0b9765c10386aa2b040bbbd54cace3b238c5
Yaroslav <doctor355@yahoo.com>
2019-06-17 19:42:20 +0300

    Before create separate folder

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -8,8 +8,22 @@ const Menu = (props) => {
   const item = props.data.map((item, index)=>(
      <MenuItem key={index} name={item} />
   ));
+
+    const addActive = e => {
+      e.preventDefault();
+      const element = e.target.className;
+      const elements = document.getElementsByClassName("menu__link");
+      console.log(elements)
+      elements.map(el => 
+        el.classList.contains('menu__link__active') ? el.classList.remove("menu__link__active") : true
+      );
+     
+      element.classList.add("menu__link__active");
+
+    };
+
   return (
-    <div className="menu">
+    <div className="menu" onClick={addActive}>
       {item}
     </div>
   );
